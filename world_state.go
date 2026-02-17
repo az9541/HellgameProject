db1bdd55bd06e3dc3321ed9a9339b9230992b53b
az9541 <az9541@mail.ru>
2026-02-17 17:19:43 +0500

    Удалены закомментированные функции copyFactionStates и copyDomainStates для улучшения читаемости кода.

--- world_state.go
+++ world_state.go
@@ -1,22 +1,5 @@
 package main
 
-// copyFactionStates создаёт глубокую копию состояний всех фракций
-/*func (sim *WorldSimulator) copyFactionStates() map[string]*FactionState {
-	copy := make(map[string]*FactionState)
-	for id, faction := range sim.State.Factions {
-		copy[id] = &FactionState{
-			ID:            faction.ID,
-			Name:          faction.Name,
-			Power:         faction.Power,
-			Territory:     faction.Territory,
-			DomainsHeld:   append([]string{}, faction.DomainsHeld...),
-			Attitude:      faction.Attitude,
-			Resources:     faction.Resources,
-			MilitaryForce: faction.MilitaryForce,
-		}
-	}
-	return copy
-}*/
 func (sim *WorldSimulator) copyFactionStates() map[string]*FactionState {
 	result := make(map[string]*FactionState)
 	for id, faction := range sim.State.Factions {
@@ -44,22 +27,6 @@ func (sim *WorldSimulator) copyFactionStates() map[string]*FactionState {
 	return result
 }
 
-// copyDomainStates создаёт глубокую копию состояний всех доменов
-/*func (sim *WorldSimulator) copyDomainStates() map[string]*DomainState {
-	copy := make(map[string]*DomainState)
-	for id, domain := range sim.State.Domains {
-		copy[id] = &DomainState{
-			ID:           domain.ID,
-			Name:         domain.Name,
-			Stability:    domain.Stability,
-			ControlledBy: domain.ControlledBy,
-			DangerLevel:  domain.DangerLevel,
-			Population:   domain.Population,
-			Mood:         domain.Mood,
-		}
-	}
-	return copy
-}*/
 // copyDomainStates создаёт глубокую копию состояний всех доменов
 func (sim *WorldSimulator) copyDomainStates() map[string]*DomainState {
 	result := make(map[string]*DomainState) // ← переименуй! copy → result
