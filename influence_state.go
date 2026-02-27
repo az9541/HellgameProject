2a6c9a9fd98afee1b912c24577fd323058c9bb4c
az9541 <az9541@mail.ru>
2026-02-27 18:12:22 +0500

    Функция Clone переименована в Copy и перенесена в соответствующий файл copy_helpers.go

--- influence_state.go
+++ influence_state.go
@@ -19,22 +19,6 @@ func BuildInfluenceState(factionIDs []string, domains []*DomainState) InfluenceS
 	return state
 }
 
-// Clone создает глубокую копию InfluenceState
-func (state InfluenceState) Clone() InfluenceState {
-	if state == nil {
-		return nil
-	}
-	out := make(InfluenceState, len(state))
-	for factionID, domainInfluence := range state {
-		domainInfluenceCopy := make(map[string]float64, len(domainInfluence))
-		for domainID, influence := range domainInfluence {
-			domainInfluenceCopy[domainID] = influence
-		}
-		out[factionID] = domainInfluenceCopy
-	}
-	return out
-}
-
 // ApplyInfluenceStateToDomains применяет данные из InfluenceState обратно в структуру доменов, обновляя влияние каждой фракции на каждый домен.
 func ApplyInfluenceStateToDomains(state InfluenceState, factionIDs []string, domains []*DomainState) {
 	for _, domain := range domains { // Походимся по каждому домену
